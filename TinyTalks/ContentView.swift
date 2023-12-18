8173106625c847296ac5537ccaf249bf21878a8f
Afrah-saleh <afrahsaleh49@hotmail.com>
2023-12-18 11:04:55 +0300

    Initial Commit

---
+++ TinyTalks/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TinyTalks
+//
+//  Created by Afrah Saleh on 05/06/1445 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TinyTalks/TinyTalksApp.swift
@@ -0,0 +1,17 @@
+//
+//  TinyTalksApp.swift
+//  TinyTalks
+//
+//  Created by Afrah Saleh on 05/06/1445 AH.
+//
+
+import SwiftUI
+
+@main
+struct TinyTalksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
